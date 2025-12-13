0de7213458eb3f9200419ca48f34478b0617f53e
Simeon Rediger <simeonrediger@gmail.com>
2025-12-13 17:56:14 -0500

    Use demo data instead of request data

--- src/index.js
+++ src/index.js
@@ -4,18 +4,21 @@ import './styles/layout.css';
 
 import visualCrossingApiKey from './api-key.js';
 
+import demoData from './demo-data.json';
+
 const weatherData = await getWeatherData('Boston');
 console.log(weatherData);
 
 async function getWeatherData(location) {
-    const requestUrl = getWeatherApiRequestUrl(location);
-    const response = await fetch(requestUrl);
+    // const requestUrl = getWeatherApiRequestUrl(location);
+    // const response = await fetch(requestUrl);
 
-    if (!response.ok) {
-        await handleBadReponse(response);
-    }
+    // if (!response.ok) {
+    //     await handleBadReponse(response);
+    // }
 
-    const data = await response.json();
+    // const data = await response.json();
+    const data = demoData;
     return data;
 }
 
