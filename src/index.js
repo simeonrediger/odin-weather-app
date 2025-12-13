08c9b4dc4ba9db67a9ad0311850a5d21bcc8fd76
Simeon Rediger <simeonrediger@gmail.com>
2025-12-13 17:50:31 -0500

    Get weather data API request URL

--- src/index.js
+++ src/index.js
@@ -2,7 +2,23 @@ import './styles/reset.css';
 import './styles/colors.css';
 import './styles/layout.css';
 
+import visualCrossingApiKey from './api-key.js';
+
 const weatherData = await getWeatherData('Boston');
 console.log(weatherData);
 
-async function getWeatherData(location) {}
+async function getWeatherData(location) {
+    const requestUrl = getWeatherApiRequestUrl(location);
+}
+
+function getWeatherApiRequestUrl(location) {
+    const apiOrigin = 'https://weather.visualcrossing.com';
+    const apiBaseUrl = '/VisualCrossingWebServices/rest/services/timeline/';
+    const apiRequestUrl = new URL(apiBaseUrl + location, apiOrigin);
+
+    apiRequestUrl.search = new URLSearchParams({
+        key: visualCrossingApiKey,
+    });
+
+    return apiRequestUrl;
+}
