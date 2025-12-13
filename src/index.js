c568e70fdb52d08abbaeea78e210048ec470d721
Simeon Rediger <simeonrediger@gmail.com>
2025-12-13 17:48:05 -0500

    Introduce getWeatherData()

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import './styles/reset.css';
 import './styles/colors.css';
 import './styles/layout.css';
+
+const weatherData = await getWeatherData('Boston');
+console.log(weatherData);
+
+async function getWeatherData(location) {}
