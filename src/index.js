e903d1105be839576f26d4171065256178e09beb
Simeon Rediger <simeonrediger@gmail.com>
2025-12-14 15:30:23 -0500

    Update location and temperature state on submit

--- src/index.js
+++ src/index.js
@@ -5,11 +5,14 @@ import '@/shared/styles/layout.css';
 import weatherComponent from './weather/component.js';
 import weatherFormComponent from './weather-form/component.js';
 import weatherService from './services/weather.js';
+import weatherState from './state/weather.js';
 
 weatherComponent.init(document);
 weatherFormComponent.init(document, handleWeatherFormSubmit);
 
 async function handleWeatherFormSubmit(data) {
     const weatherData = await weatherService.getData(data.location);
+    weatherState.location = weatherData.resolvedAddress;
+    weatherState.temperature = weatherData.currentConditions.temp;
     weatherComponent.render(weatherData, data.useFahrenheit);
 }
