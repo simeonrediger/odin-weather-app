7d0ba0524ba51da67682b44fb1dbe3bdbc9bad9a
Simeon Rediger <simeonrediger@gmail.com>
2025-12-16 13:10:59 -0500

    Simplify element variable names

--- src/features/weather/components/weather-view/view.js
+++ src/features/weather/components/weather-view/view.js
@@ -6,16 +6,16 @@ import assert from '@/shared/utils/assert.js';
 
 let container;
 
-let resultsContainer;
-let iconElement;
+let results;
+let weatherIcon;
 let locationElement;
 let temperatureElement;
 
-let loadingIndicatorElement;
+let loadingIndicator;
 
 let errorContainer;
 let errorMessageElement;
-let errorIconElement;
+let errorIcon;
 
 let currentWeatherClass;
 
@@ -28,31 +28,31 @@ function cacheElements(root) {
     container = root.querySelector("[data-role='weather-view']");
     assert.elements({ container });
 
-    resultsContainer = container.querySelector("[data-role='results']");
+    results = container.querySelector("[data-role='results']");
     locationElement = container.querySelector("[data-role='location']");
     temperatureElement = container.querySelector("[data-role='temperature']");
-    iconElement = container.querySelector("[data-role='icon']");
+    weatherIcon = container.querySelector("[data-role='icon']");
 
-    loadingIndicatorElement = container.querySelector(
+    loadingIndicator = container.querySelector(
         "[data-role='loading-indicator']",
     );
 
     errorContainer = container.querySelector("[data-role='error']");
-    errorIconElement = container.querySelector("[data-role='error-icon']");
+    errorIcon = container.querySelector("[data-role='error-icon']");
     errorMessageElement = container.querySelector(
         "[data-role='error-message']",
     );
 
     assert.elements({
-        resultsContainer,
+        results,
         locationElement,
         temperatureElement,
-        iconElement,
+        weatherIcon,
 
-        loadingIndicatorElement,
+        loadingIndicator,
 
         errorContainer,
-        errorIconElement,
+        errorIcon,
         errorMessageElement,
     });
 }
@@ -67,7 +67,7 @@ function render({ location, temperature, useFahrenheit, iconId }) {
 
 async function renderIcon({ iconId }) {
     const { default: icon } = await import(`./icons/${iconId}.svg`);
-    iconElement.src = icon;
+    weatherIcon.src = icon;
 }
 
 function renderLocation({ location }) {
@@ -92,7 +92,7 @@ function renderBackground({ weatherClass } = {}) {
 
 async function renderErrorIcon() {
     const { default: icon } = await import(`./icons/error.svg`);
-    errorIconElement.src = icon;
+    errorIcon.src = icon;
 }
 
 function renderError(message) {
@@ -103,19 +103,19 @@ function renderError(message) {
 
 function showResults() {
     errorContainer.classList.add('hidden');
-    loadingIndicatorElement.classList.add('hidden');
-    resultsContainer.classList.remove('hidden');
+    loadingIndicator.classList.add('hidden');
+    results.classList.remove('hidden');
 }
 
 function showLoadingIndicator() {
-    resultsContainer.classList.add('hidden');
+    results.classList.add('hidden');
     errorContainer.classList.add('hidden');
-    loadingIndicatorElement.classList.remove('hidden');
+    loadingIndicator.classList.remove('hidden');
 }
 
 function showError() {
-    resultsContainer.classList.add('hidden');
-    loadingIndicatorElement.classList.add('hidden');
+    results.classList.add('hidden');
+    loadingIndicator.classList.add('hidden');
     errorContainer.classList.remove('hidden');
 }
 
